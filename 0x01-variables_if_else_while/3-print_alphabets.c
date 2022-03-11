4aec3c63e4ce82c927165997612be062b72beba6
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-11 13:57:09 -0800

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'a';
+	char U = 'A';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		++l;
+	}
+	while (U <= 'Z')
+	{
+		putchar(U);
+		++U;
+	}
+	putchar('\n');
+
+	return (0);
+}
