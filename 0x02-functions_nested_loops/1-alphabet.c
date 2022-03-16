b74ac4cb8a6e3e83164764ae50f841b6fa4f81a3
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-16 11:49:33 -0700

    functions nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main -Entry point
  *
- * print_alphabet - prints lowercase alphabets
+ * Discription: print_alphabet - prints lowercase alphabets
  *
  * Return: void
  */
@@ -12,8 +12,6 @@ void print_alphabet(void)
 	int A;
 	
 	for (A = 'a'; A <= 'z'; ++A)
-	{
 		_putchar(A);
-	}
 	_putchar('\n');
 }
