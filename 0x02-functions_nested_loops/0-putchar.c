b4028a4eab55fe477f1402ce0e65cdda5b448751
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-16 09:35:30 -0700

    functions_nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * main entry point
+ * main entry poinit
+ *
+ * discrition prints _putchar
  *
  * return:always 0()success
  */
@@ -9,7 +11,9 @@
 int main(void)
 {
 	char *str = "_putchar\n";
+
 	while (*str != '\0')
 		_putchar(*str++);
+
 	return (0);
 }
