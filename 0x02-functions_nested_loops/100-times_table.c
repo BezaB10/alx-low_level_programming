88a2c4bd56048364b08281be000a016a10bd0ba8
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-16 16:39:01 -0700

    functions nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,6 +33,11 @@ void print_times_table(int n)
 				else if (z <= 99)
 				{
 					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
 					_putchar(((z / 100) % 10) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
