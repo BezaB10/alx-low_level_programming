344f305554883255323d9513fece6ecc5fc9c197
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-16 16:10:18 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @n: integer input number
+ *
+ * Return: the result of addition of n and m
+ */
+
+int add(int n, int m)
+{
+	int a;
+
+	a = n+ m;
+
+	return (a);
+}
