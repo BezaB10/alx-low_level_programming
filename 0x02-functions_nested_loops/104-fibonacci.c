0f276e4913cc1247d1b4b5662f641dc9490b6ce7
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-16 17:55:20 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int n, x, y, z;
+	int a;
+
+	x = 1;
+	y = 2;
+
+	printf("%ld, ", x);
+	printf("%ld, ", y);
+	for (a = 0; a < 96; a++)
+	{
+		z = x + y;
+		if (z > 0)
+		{
+			printf("%ld", z);
+		}
+		else if (z < 0)
+		{
+			n = z * -1;
+			printf("%ld", n);
+		}
+		else
+		{
+			printf(" ");
+		}
+		x = y;
+		y = z;
+
+		if (a == 95)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
