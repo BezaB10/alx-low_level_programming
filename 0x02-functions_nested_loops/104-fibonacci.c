cadf11f518429160cdda1f7e2bfdb688bd3fcbb0
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-19 05:58:42 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,42 +20,38 @@ int numLength(int num)
 		num = num / 10;
 		length += 1;
 	}
-	return (length);
+	return (length)
 }
-
 int main(void)
 {
-	long int n, x, y, z;
-	int a;
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	x = 1;
-	y = 2;
-
-	printf("%ld, ", x);
-	printf("%ld, ", y);
-	for (a = 0; a < 96; a++)
+	for (count = 1; count <= 98; ++count)
 	{
-		z = x + y;
-		if (z > 0)
-		{
-			printf("%ld", z);
-		}
-		else if (z < 0)
-		{
-			n = z * -1;
-			printf("%ld", n);
-		}
-		else
-		{
-			printf(" ");
-		}
-		x = y;
-		y = z;
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		if (a == 95)
-			printf("\n");
-		else
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			--initial0s;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
 			printf(", ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
