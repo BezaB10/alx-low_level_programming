27aa7a35b515a0ee82782c4e26d1a1bd922eabd3
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-19 06:20:23 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,41 +6,41 @@
  *
  * Description: prints the first 98 Fibonacci numbers.
  *
- * numLength - returns the length of string
+ * NL: returns the length of string
  *
  * @num: operand number
  *
  * Return: Always 0 (Success)
  */
-int numLength(int num)
+int NL(int N)
 {
-	int length = 0;
+	int L = 0;
 
-	if (!num)
+	if (!L)
 		return (1);
-	while (num)
+	while (N)
 	{
-		num = num / 10;
-		length += 1;
+		N = N / 10;
+		L += 1;
 	}
-	return (length);
+	return (L);
 }
 
 int main(void)
 {
-	int count, initial0s;
+	int c, initials;
 	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	for (count = 1; count <= 98; ++count)
+	for (c = 1; c <= 98; ++c)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
+		initials = NL(mx) - 1 - NL(f1);
 
-		while (f1o > 0 && initial0s > 0)
+		while (f1o > 0 && initials > 0)
 		{
 			printf("%d", 0);
-			--initial0s;
+			--initials;
 		}
 
 		printf("%lu", f1);
@@ -52,7 +52,7 @@ int main(void)
 		f2 = sum;
 		f2o = sumo;
 
-		if (count != 98)
+		if (c != 98)
 			printf(", ");
 		else
 			printf("\n");
