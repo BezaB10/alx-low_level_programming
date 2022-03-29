7969a7d81105cd7dfb1b999000cb9b759bdd11ae
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-29 13:21:45 -0700

    pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int x;
-
-	for (x = 0; s[x] != '\0'; x++)
+	while (*s != '\0')
 	{
-		if (s[x] == c)
-		{
-			return (s + x);
-		}
+		if (*s == c)
+			return (s);
+		s++;
 	}
+	if (*s == c)
+		return (s);
 	return ('\0');
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to the string
+ * @accept: pointer to the number of bytes of substring
+ * Return: the number of bytes of the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int x;
+	int y = 0;
+	int length = 0;
+
+	for (x = 0; s[x] != '\0' && s[x] != 32; x++)
+	{
+		if (accept[y] != s[x])
+		{
+			for (; accept[y] != '\0'; y++)
+			{
+				length++;
+			}
+			if (accept[y]  == '\0')
+			{
+				break;
+			}
+		}
+	}
+	return (length);
+}
