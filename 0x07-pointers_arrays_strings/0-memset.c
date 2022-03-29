66e8502cc5fa0d791b0b81f0a04458fb11537f10
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-03-29 13:08:09 -0700

    pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,15 +12,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * declare an unsigned int because we are storing a
-	 * value that will always be non-negative (zero or 
-	 * positive)
-	 */
-	unsigned int i;
+	unsigned int x = 0;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
+	for (; x < n; x++)
+	{
+		s[x] = b;
+	}
 	return (s);
 }
