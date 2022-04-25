4ce2a84e6429a1524f015e38e64428e807f2beb7
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-04-25 14:22:35 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef LISTS_H_
-#define LISTS_H_
-
+#ifndef _S_LINK_LISTS_
+#define _S_LINK_LISTS_
+#include <stddef.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,12 +11,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +34,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
