bc1edf35ac8d3c82978de1707ab4aeebb87dd5da
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-04-26 07:41:13 -0700

    edited

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
+
 
 /**
   * print_listint - Prints all the elements of a linked list
@@ -7,6 +9,7 @@
   *
   * Return: The number of nodes of the linked list
   */
+
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
