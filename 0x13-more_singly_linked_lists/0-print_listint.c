1a8ce63a0f9946fd92d2b39f754ebeab1b9f0d8c
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-04-26 07:26:40 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,32 +1,25 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - a function that prints all elements of a list
- *
- * @h: pointer to first node
- *
- * Return: the number of nodes
-*/
+  * print_listint - Prints all the elements of a linked list
+  * @h: The head of the linked list
+  *
+  * Return: The number of nodes of the linked list
+  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+	int count = 0;
 
-	/* return 0 as no of nodes when h is null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-
-		/*go to the next node*/
-		h = h->next;
-		/*count no of nodes*/
-		node_count++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
 
-	/*print last node*/
-	printf("%d\n", h->n);
-
-	return (node_count);
+	return (count);
 }
