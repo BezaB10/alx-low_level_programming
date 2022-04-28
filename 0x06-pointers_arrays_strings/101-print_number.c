ab78bb0ab518c03be2ebc4bb334743b55fee9e82
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-04-28 07:40:03 -0700

    edited

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_number - print integers
- * @n: integer value.
- * Return: nothing
+ * print_number - prints an integer
+ *
+ * @n: integer to be printed
+ *
  */
 
 void print_number(int n)
@@ -12,12 +13,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('_');
+		_putchar('-');
 		x = -x;
 	}
 	if ((x / 10) > 0)
 	{
 		print_number(x / 10);
 	}
-	putchar((x % 10) + '0');
+	_putchar((x % 10) + '0');
 }
