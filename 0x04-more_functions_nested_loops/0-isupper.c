58827c9ae9897f3e095cdd9ad5ba1009d5d37a53
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-05-22 08:54:33 -0700

    edited

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
-#include "main.h"
-
 /**
- * _isupper - This function checks for uppercase character
- * @c: The character to check
- * Return: 1 if c is uppercase, otherwise 0
- */
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
-
 }
