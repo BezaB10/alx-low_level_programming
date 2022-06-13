40bdf666d8f1622b148b8986480b8a8411e48b89
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-06-13 12:54:35 -0700

    tried

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc *.c -fPIC -shared -o libruin.so
-LD_PRELOAD=$PWD/library.so
--- 0x18-dynamic_libraries/fake.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int dprintf(int fd, const char *format, ...){return (printf("Congratulations, you win the Jackpot!%c", 10));}
-ssize_t write(int fd, const void *buf, size_t count)
-{return (write(2,"Congratulations, you win the Jackpot!", 38));}
