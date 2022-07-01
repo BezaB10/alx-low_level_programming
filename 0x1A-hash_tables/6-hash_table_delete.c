8128bb302d13a9c0018f42e38e321ecd22d7f4a5
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-01 13:00:31 -0700

    try

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,28 +1,29 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_delete - deletes a hash table.
-  * @ht: hash table to be deleted.
-  *
-  */
+ * hash_table_delete - frees a hash table
+ *
+ * @ht: hash table to be freed
+ *
+ * Return: void
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp, *temp2;
-	unsigned long int i = 0;
+	hash_node_t *strider = NULL, *freeman = NULL;
+	unsigned long int i;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
-
 	for (i = 0; i < ht->size; i++)
 	{
-		temp = ht->array[i];
-		while (temp != NULL)
+		strider = ht->array[i];
+		while (strider)
 		{
-			temp2 = temp->next;
-			free(temp->key);
-			free(temp->value);
-			free(temp);
-			temp = temp2;
+			freeman = strider;
+			strider = strider->next;
+			free(freeman->value);
+			free(freeman);
 		}
 	}
 	free(ht->array);
