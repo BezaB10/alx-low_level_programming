ab8f2b40ff900e889e01e21f0ff078f0692c5e67
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-01 13:06:49 -0700

    hh

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -28,3 +28,25 @@ void hash_table_delete(hash_table_t *ht)
 	free(ht->array);
 	free(ht);
 }
+
+
+/**
+ * free_hash_list - frees a hash_node_t list
+ * @head: head of linked list
+ */
+void free_hash_list(hash_node_t *head)
+{
+	hash_node_t *current;
+	hash_node_t *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->key);
+		free(current->value);
+		free(current);
+		current = next;
+	}
+}
